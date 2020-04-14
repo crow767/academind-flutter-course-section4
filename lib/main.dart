4fe4a5de692b566036ae895d1f76e7cf9d91bc99
a-o76 <at-martins@outlook.com>
2020-04-13 22:01:57 -0300

    Section 4 - Class 77

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,14 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transactions = [
-    Transaction(id: '1', title: 'Cheese Pizza', date: DateTime.now(), amount: 12.01),
-    Transaction(id: '2', title: 'Stella Artois Beer', date: DateTime.now(), amount: 4.11)
+    Transaction(
+        id: '1', title: 'Cheese Pizza', date: DateTime.now(), amount: 12.01),
+    Transaction(
+        id: '2',
+        title: 'Stella Artois Beer',
+        date: DateTime.now(),
+        amount: 4.11)
   ];
 
   @override
@@ -37,9 +41,55 @@ class MyHomePage extends StatelessWidget {
                   color: Colors.blue,
                 ),
               ),
-              Card(
-                color: Colors.red,
-                child: Text('LIST OF TX'),
+              Column(
+                children: transactions.map((tx) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          width: 80,
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          child: Center(
+                            child: Text(
+                              tx.amount.toString(),
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.deepPurple,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            width: 2,
+                            color: Colors.deepPurple,
+                          )),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(tx.date.toLocal().toString(),
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ))
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }).toList(),
               )
             ],
           ),
