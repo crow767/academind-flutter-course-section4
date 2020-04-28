68146008a9b49d8f1588709d3ded077c3a3c520a
Augusto Martins <at-martins@outlook.com>
2020-04-27 21:08:35 -0300

    Section 4 - Class 103

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -29,16 +30,36 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    // change a list to another type
+    return groupedTransactions.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+
+      /// ???
+    }); // starting value, function
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Card(
         elevation: 6,
         margin: EdgeInsets.all(6),
-        child: Row(
-          children: groupedTransactions.map((tx) {
-            return Text('${tx['day']}:${tx['amount']}');
-          }).toList(),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: groupedTransactions.map((data) {
+              return Flexible(
+                  fit: FlexFit.tight,
+                  child: ChartBar(
+                      data['day'],
+                      data['amount'],
+                      totalSpending == 0.0
+                          ? 0.0
+                          : totalSpending / data['amount']));
+            }).toList(),
+          ),
         ),
       ),
     );
